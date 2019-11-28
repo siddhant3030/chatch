b8ff5ab676bbe032562efa6de384ade5f7a93aea
siddhant3030 <ssiddhant3030@gmail.com>
2019-11-28 19:36:03 +0530

    room channel

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,48 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import { Socket, Presence } from "phoenix"
+
+let user = document.getElementById("user").innerText
+let socket = new Socket("/socket", { params: { user: user } })
+socket.connect()
+
+let presences = {}
+
+let formatedTimestamp = (Ts) => {
+    let date = new Date(Ts)
+    return date.toLocaleString()
+}
+
+let listBy = (user, { metas: metas }) => {
+    return {
+        user: user,
+        onlineAt: formatedTimestamp(metas[0].online_at)
+    }
+}
+
+let userList = document.getElementById("UserList")
+let render = (presences) => {
+    userList.innerHTML = Presence.list(presences, listBy)
+        .map(presence => `
+      <li>
+        <b>${presence.user}</b>
+        <br><small>online since ${presence.onlineAt}</small>
+      </li>
+    `)
+        .join("")
+}
+
+let room = socket.channel("room:lobby", {})
+room.on("presence_state", state => {
+    presences = Presence.syncState(presences, state)
+    render(presences)
+})
+
+room.on("presence_diff", diff => {
+    presences = Presence.syncDiff(presences, diff)
+    render(presences)
+})
+
+room.join()
---
+++ lib/chatter_web/channels/room_channel.ex
@@ -0,0 +1,18 @@
+defmodule ChatterWeb.RoomChannel do
+  use Chatter.Web, :channel
+  alias ChatterWeb.Presence
+
+
+  def join("room:lobby", _, socket) do
+    send self(), :after_join
+    {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    Presence.track(socket, socket.assigns.user, %{
+      online_at: :os.system_time(:milli_seconds)
+    })
+    push socket, "presence_state", Presence.list(socket)
+    {:noreply, socket}
+  end
+end
